e91644772e7b55566424481b359edd909882b33f
alegonshu <alegonshu@gmail.com>
2024-09-14 19:36:48 -0400

    side bar

--- app/sidebar.tsx
+++ app/sidebar.tsx
@@ -1,14 +1,44 @@
 import React from "react";
-import "./Sidebar.css";
 
 function Sidebar() {
   return (
-    <div className="sidebar">
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Topics</a></li>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Settings</a></li>
+    <div className="w-20 bg-gray-100 p-4 border-r flex flex-col h-screen">
+      {/* Icon at the very top */}
+      {/* <div className="flex justify-center mb-8">
+        <a href="#" className="text-gray-700 hover:text-blue-500">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 w-8 mb-2">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M12 6.042A8.967 8.967 0 0 0 6 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 0 1 6 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 0 1 6-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0 0 18 18a8.967 8.967 0 0 0-6 2.292m0-14.25v14.25" />
+          </svg>
+          <span className="sr-only">Top Icon</span>
+        </a>
+      </div> */}
+
+      {/* Icons in the middle */}
+      <ul className="flex-1 flex flex-col space-y-6 justify-center">
+        <li className="flex justify-center">
+          <a href="#" className="flex flex-col items-center text-gray-700 hover:text-blue-500">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 w-8 mb-2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+            </svg>
+            <span className="sr-only">Home</span>
+          </a>
+        </li>
+        <li className="flex justify-center">
+          <a href="#" className="flex flex-col items-center text-gray-700 hover:text-blue-500">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 w-8 mb-2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M18 18.72a9.094 9.094 0 0 0 3.741-.479 3 3 0 0 0-4.682-2.72m.94 3.198.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0 1 12 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 0 1 6 18.719m12 0a5.971 5.971 0 0 0-.941-3.197m0 0A5.995 5.995 0 0 0 12 12.75a5.995 5.995 0 0 0-5.058 2.772m0 0a3 3 0 0 0-4.681 2.72 8.986 8.986 0 0 0 3.74.477m.94-3.197a5.971 5.971 0 0 0-.94 3.197M15 6.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm6 3a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Zm-13.5 0a2.25 2.25 0 1 1-4.5 0 2.25 2.25 0 0 1 4.5 0Z" />
+            </svg>
+            <span className="sr-only">Characters</span>
+          </a>
+        </li>
+        <li className="flex justify-center">
+          <a href="#" className="flex flex-col items-center text-gray-700 hover:text-blue-500">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-8 w-8 mb-2">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+            </svg>
+            <span className="sr-only">Add</span>
+          </a>
+        </li>
       </ul>
     </div>
   );
