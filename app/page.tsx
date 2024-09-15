225aeb07366c2c8b0be98d652d8d21258a8474a1
Anna Xu <anna.xu@yale.edu>
2024-09-14 20:39:38 -0400

    merge

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,8 @@
 import { useAction } from "convex/react";
 import { api } from "../convex/_generated/api";
 import Image from "next/image";
+import { Chapter } from "./chapters/Chapter";
+import LandingPage from "./landing/LandingPage";
 import { Chapter } from "./chapters/chapter";
 
 export default function Home() {
@@ -12,7 +14,7 @@ export default function Home() {
     <div className="grid min-h-screen flex items-center justify-center font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col row-start-2 justify-center items-center ">
         {/* Render the ChapterSlice component with appropriate props */}
-        <Chapter
+        {/* <Chapter
           chapterNumber={1}
           storyTitle="My Story"
           authors="Author Name"
@@ -21,7 +23,8 @@ export default function Home() {
             "https://via.placeholder.com/600x400",
             "https://via.placeholder.com/600x400"
           ]}
-        />
+        /> */}
+        <LandingPage />
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center"></footer>
     </div>
