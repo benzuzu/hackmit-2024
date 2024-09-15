720e4a71016243a128685e057ce7de592590e383
Addison Goolsbee <addisongoolsbee@gmail.com>
2024-09-15 10:57:25 -0400

    stuf

--- app/components/chapters/chapter.tsx
+++ app/components/chapters/chapter.tsx
@@ -1,6 +1,10 @@
-import React from "react";
+"use client"
+
+import React, { useState } from "react";
 import { ChapterBanner } from "./ChapterBanner";
 import { ChapterSlice } from "./chapterslice";
+import { ArrowRightIcon, ArrowLeftIcon } from "@heroicons/react/solid";
+
 
 interface ChapterProps {
   chapterNumber: number;
@@ -10,6 +14,24 @@ interface ChapterProps {
 }
 
 export function Chapter({ chapterNumber, text, imageUrls }: ChapterProps) {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [currentChapter, setCurrentChapter] = useState<number | null>(null);
+
+  const handlePrevPage = () => {
+    if (currentPage > 0) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  // Handler to go to the next page
+  const handleNextPage = () => {
+    if (currentPage < text.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  // Fetch chapter data from API (mock function)
+  
   return (
     <div className="grid grid-rows-[auto_auto_auto] items-start justify-items-center min-h-screen p-8 pb-20 gap-8 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-1 items-center sm:items-start">
